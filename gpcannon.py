7737e975e5822d5ee196c0ecf7b39030d449d39e
Dan F-M <danfm@nyu.edu>
2015-03-10 12:14:18 +0100

    optimize for all three hyperparams

--- gpcannon.py
+++ gpcannon.py
@@ -4,11 +4,12 @@ from __future__ import division, print_function
 
 __all__ = []
 
+import os
 import pickle
 import numpy as np
-from itertools import product
 import matplotlib.pyplot as pl
 from scipy.linalg import cho_factor, cho_solve
+import scipy.optimize as op
 
 f_all = pickle.load(open("data/norm_tr_fluxes.p", "r"))
 l_all = pickle.load(open("data/tr_label_vals.p", "r"))
@@ -30,7 +31,7 @@ var = 1./ivar[m]
 
 # Covariance function
 def kernel(theta, l_i, l_j, white_noise=True):
-    a, tau, s = theta
+    a, tau, s = np.exp(theta)
     K = a**2 * np.exp(-0.5*(l_i-l_j)**2/tau**2)
     if white_noise:
         K[np.diag_indices_from(K)] += s**2
@@ -62,33 +63,26 @@ def predictive_mean(theta, test_labels):
     alpha = compute_l_and_alpha(theta)[1]
     return np.dot(K_star, alpha)
 
-a = np.linspace(-8, 0, 50)
-a = 10**a
-taus = np.linspace(-3, 3, 51)
-taus = 10**taus
-# a = 0.0075
-# tau = 10.
-s = np.linspace(-3, 0, 52)
-s = 10**s
-likelihoods = np.zeros(len(taus))
+# Evaluate the likelihood of
 
-for i, tau_val in enumerate(taus):
-    likelihoods[i] = ln_likelihood([a, tau_val, s])
+hyperfn = "best_hyperparams.p"
+if not os.path.exists(hyperfn):
+    nll = lambda theta: -ln_likelihood(theta)
 
-pl.scatter(taus, likelihoods)
-# pl.xscale("log")
-# pl.yscale("log")
+    # Optimize
+    bounds = np.log([(0.001, 0.1), (10, 1000), (0.0001, 0.1)])
+    output = op.minimize(nll, np.log([0.0075, 400, 0.005]), method="L-BFGS-B",
+                         bounds=bounds)
+    best_hyperparams = output.x
+    pickle.dump(best_hyperparams, open(hyperfn, "wb"), -1)
+
+    test_labels = np.linspace(np.min(l), np.max(l), 2000)
+    mu = predictive_mean(best_hyperparams, test_labels)
+    pl.scatter(l, f, c="k")
+    pl.plot(test_labels, mu, "g", lw=2)
+    pl.show()
+
+else:
+    best_hyperparams = pickle.load(open(hyperfn, "r"))
 
 
-pl.figure()
-test_labels = np.linspace(np.min(l), np.max(l), 2000)
-
-# i, j = np.unravel_index(np.argmax(likelihoods), likelihoods.shape)
-# amax = a[i]
-# taumax = taus[j]
-# print(amax, taumax)
-
-# mu = predictive_mean([a, tau, s[np.argmax(likelihoods)]], test_labels)
-# pl.scatter(l, f, c="k")
-# pl.plot(test_labels, mu, "g", lw=2)
-pl.show()
