6edbd2ac4d0c995fa3b398deefdfd183135c26f5
Dan F-M <danfm@nyu.edu>
2015-03-10 11:40:12 +0100

    adding scatter as param to kernel

--- gpcannon.py
+++ gpcannon.py
@@ -29,10 +29,11 @@ var = 1./ivar[m]
 
 
 # Covariance function
-def kernel(theta, l_i, l_j):
-    a, tau = theta
+def kernel(theta, l_i, l_j, white_noise=True):
+    a, tau, s = theta
     K = a**2 * np.exp(-0.5*(l_i-l_j)**2/tau**2)
-    K[np.diag_indices_from(K)] += 0.004
+    if white_noise:
+        K[np.diag_indices_from(K)] += s**2
     return K
 
 
@@ -57,7 +58,7 @@ def ln_likelihood(theta):
 
 # Evaluate the predictive mean
 def predictive_mean(theta, test_labels):
-    K_star = kernel(theta, test_labels[:, None], l[None, :])
+    K_star = kernel(theta, test_labels[:, None], l[None, :], white_noise=False)
     alpha = compute_l_and_alpha(theta)[1]
     return np.dot(K_star, alpha)
 
@@ -65,26 +66,29 @@ a = np.linspace(-8, 0, 50)
 a = 10**a
 taus = np.linspace(-3, 3, 51)
 taus = 10**taus
-likelihoods = np.zeros((len(a), len(taus)))
+# a = 0.0075
+# tau = 10.
+s = np.linspace(-3, 0, 52)
+s = 10**s
+likelihoods = np.zeros(len(taus))
 
-for xi, yi in product(range(len(a)), range(len(taus))):
-    likelihoods[xi, yi] = ln_likelihood([a[xi], taus[yi]])
+for i, tau_val in enumerate(taus):
+    likelihoods[i] = ln_likelihood([a, tau_val, s])
 
-pl.pcolor(a, taus, np.log(likelihoods.T), cmap="gray_r")
-pl.xscale("log")
-pl.yscale("log")
-pl.colorbar()
-pl.show()
+pl.scatter(taus, likelihoods)
+# pl.xscale("log")
+# pl.yscale("log")
 
 
-# pl.figure()
-# test_labels = np.linspace(np.min(l), np.max(l), 2000)
+pl.figure()
+test_labels = np.linspace(np.min(l), np.max(l), 2000)
 
 # i, j = np.unravel_index(np.argmax(likelihoods), likelihoods.shape)
 # amax = a[i]
 # taumax = taus[j]
 # print(amax, taumax)
 
-# mu = predictive_mean([amax, taumax], test_labels)
+# mu = predictive_mean([a, tau, s[np.argmax(likelihoods)]], test_labels)
 # pl.scatter(l, f, c="k")
 # pl.plot(test_labels, mu, "g", lw=2)
+pl.show()
