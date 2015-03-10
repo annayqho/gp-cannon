d8cc5a0488c5bd1544c41272edcfd17f92e002f5
annayqho <annayqho@gmail.com>
2015-03-10 18:56:42 +0100

    optimizing over 3D of labels

--- gpcannon.py
+++ gpcannon.py
@@ -9,39 +9,48 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as pl
 from scipy.linalg import cho_factor, cho_solve
+from itertools import product
 import scipy.optimize as op
 
 f_all = pickle.load(open("data/norm_tr_fluxes.p", "r"))
 l_all = pickle.load(open("data/tr_label_vals.p", "r"))
 ivar_all = pickle.load(open("data/norm_tr_ivars.p", "r"))
 
-pix = 8230
-label = 0  # teff
+pix = 1383 
+# label = 2
 
 f = f_all[:, pix]
-l = l_all[:, label]
+# l = l_all[:, label]
 ivar = ivar_all[:, pix]
 
-m = f > 0.0
+# identify bad pixels. flux == 0 or flux == 1
+m = np.logical_and(f != 0.0, f != 1.0)
 f = f[m]
 f -= np.mean(f)
-l = l[m]
+l_all = l_all[m,:]
 var = 1./ivar[m]
+
+
+# at this point, the relevant arrays are: f, var, l_all
 
 
 # Covariance function
 def kernel(theta, l_i, l_j, white_noise=True):
-    a, tau, s = np.exp(theta)
-    K = a**2 * np.exp(-0.5*(l_i-l_j)**2/tau**2)
+    print(l_i.shape)
+    a, tau0, tau1, tau2, s = np.exp(theta)
+    K = a**2 * np.exp(-0.5*(((l_i-l_j)[:,:,0])**2/tau0**2 + 
+                           ((l_i-l_j)[:,:,1])**2/tau1**2 + 
+                           ((l_i-l_j)[:,:,2])**2/tau2**2))
     if white_noise:
         K[np.diag_indices_from(K)] += s**2
     return K
 
 
+
 # Calculate alpha
 def compute_l_and_alpha(theta):
     # compute the kernel
-    K = kernel(theta, l[:, None], l[None, :])
+    K = kernel(theta, l_all[:, None], l_all[None, :])
     # add in the uncertainties in the fluxes to the kernel
     K[np.diag_indices_from(K)] += var
     # factorize the kernel
@@ -59,7 +68,7 @@ def ln_likelihood(theta):
 
 # Evaluate the predictive mean
 def predictive_mean(theta, test_labels):
-    K_star = kernel(theta, test_labels[:, None], l[None, :], white_noise=False)
+    K_star = kernel(theta, test_labels[:, None], l_all[None, :], white_noise=False)
     alpha = compute_l_and_alpha(theta)[1]
     return np.dot(K_star, alpha)
 
@@ -67,7 +76,7 @@ def predictive_mean(theta, test_labels):
 # Evaluate the likelihood of a data point given model
 def test_ln_likelihood(label, flux, flux_var, theta, L, alpha):
     # Calculate the mean prediction.
-    K_star = kernel(theta, label, l, white_noise=False)
+    K_star = kernel(theta, label, l_all, white_noise=False)
     f_star = np.dot(K_star, alpha)
 
     # Calculate the variance on the prediction.
@@ -77,33 +86,47 @@ def test_ln_likelihood(label, flux, flux_var, theta, L, alpha):
 
     return -0.5 * ((flux - f_star) ** 2 / pred_var + np.log(pred_var))
 
-
 hyperfn = "best_hyperparams.p"
 if not os.path.exists(hyperfn):
+    print("fitting for hyperparams")
     nll = lambda theta: -ln_likelihood(theta)
 
     # Optimize
-    bounds = np.log([(0.001, 0.1), (10, 1000), (0.0001, 0.1)])
-    output = op.minimize(nll, np.log([0.0075, 400, 0.005]), method="L-BFGS-B",
-                         bounds=bounds)
+    bounds = np.log([(0.001, 0.1), (100, 10000), (0.01, 10), (0.01, 10),
+                     (0.0001, 0.1)])
+    output = op.minimize(nll, np.log([0.01, 100, 0.5, 0.05, 0.01]), 
+                         method="L-BFGS-B", bounds=bounds)
     best_hyperparams = output.x
+    print(np.exp(best_hyperparams))
     pickle.dump(best_hyperparams, open(hyperfn, "wb"), -1)
-
-    test_labels = np.linspace(np.min(l), np.max(l), 2000)
-    mu = predictive_mean(best_hyperparams, test_labels)
-    pl.scatter(l, f, c="k")
-    pl.plot(test_labels, mu, "g", lw=2)
 
 else:
     print("loading")
     best_hyperparams = pickle.load(open(hyperfn, "r"))
 
+test_labels = np.zeros((2000,3)) + np.max(l_all, axis=0)[None,:]
+axis = 2 
+test_labels[:,axis] = np.linspace(np.min(l_all[:,axis]), 
+                                  np.max(l_all[:,axis]), 
+                                  len(test_labels[:,axis]))
+print("finding mu")
+mu = predictive_mean(best_hyperparams, test_labels)
+pl.scatter(l_all[:,axis], f, c="k")
+pl.plot(test_labels[:,axis], mu, "g", lw=2)
+
 L, alpha = compute_l_and_alpha(best_hyperparams)
-test_labels = np.linspace(np.min(l), np.max(l), 200)
-lls = [test_ln_likelihood(tl, f[0], var[0], best_hyperparams, L, alpha)
-       for tl in test_labels]
+print("test step")
+lls = [test_ln_likelihood(test_labels[i,:], f[0], var[0], best_hyperparams, 
+       L, alpha) for i in range(0,len(test_labels))]
+
+# lls = [test_ln_likelihood(tl, f[0], var[0], best_hyperparams, L, alpha)
+#       for tl in test_labels]
 
 pl.figure()
 pl.plot(test_labels, lls)
 pl.gca().axvline(l[0])
 pl.show()
+
+# moving to three parameters
+
+
