18030a11185061a7ff67834fefaebb0258af55f2
annayqho <annayqho@gmail.com>
2015-03-12 13:25:27 +0100

    optimize label for obj 0 using 150 pix

--- gpcannon.py
+++ gpcannon.py
@@ -12,24 +12,6 @@ from scipy.linalg import cho_factor, cho_solve
 from itertools import product
 import scipy.optimize as op
 
-f_all = pickle.load(open("data/norm_tr_fluxes.p", "r"))
-l_all = pickle.load(open("data/tr_label_vals.p", "r"))
-ivar_all = pickle.load(open("data/norm_tr_ivars.p", "r"))
-
-pix = 1383 
-
-f = f_all[:, pix]
-ivar = ivar_all[:, pix]
-
-# identify bad pixels. flux == 0 or flux == 1
-m = np.logical_and(f != 0.0, f != 1.0)
-f = f[m]
-f -= np.mean(f)
-l_all = l_all[m,:]
-var = 1./ivar[m]
-
-
-# Covariance function
 def kernel(theta, l_i, l_j, white_noise=True):
     a, tau0, tau1, tau2, s = np.exp(theta)
     K = a**2 * np.exp(-0.5*(((l_i-l_j)[:,:,0])**2/tau0**2 + 
@@ -41,7 +23,7 @@ def kernel(theta, l_i, l_j, white_noise=True):
 
 
 # Calculate alpha
-def compute_l_and_alpha(theta):
+def compute_l_and_alpha(theta, f, var, l_all):
     # compute the kernel
     K = kernel(theta, l_all[:, None], l_all[None, :])
     # add in the uncertainties in the fluxes to the kernel
@@ -52,8 +34,8 @@ def compute_l_and_alpha(theta):
 
 
 # Likelihood function
-def ln_likelihood(theta):
-    (L, flag), alpha = compute_l_and_alpha(theta)
+def ln_likelihood(theta, f, var, l_all):
+    (L, flag), alpha = compute_l_and_alpha(theta, f, var, l_all)
     # compute half of the log determinant
     lndet = np.sum(np.log(np.diag(L)))
     return -0.5*np.dot(f, alpha)-lndet
@@ -69,7 +51,7 @@ def predictive_mean(theta, test_labels):
 
 
 # Evaluate the likelihood of a data point given model
-def test_ln_likelihood(label, flux, flux_var, theta, (L,flag), alpha):
+def test_ln_likelihood_single_pix(label, flux, flux_var, theta, (L,flag), alpha, l_all):
     # Calculate the mean prediction.
     K_star = kernel(theta, label[None,None,:], l_all[None,:,:], 
                     white_noise=False)
@@ -82,38 +64,110 @@ def test_ln_likelihood(label, flux, flux_var, theta, (L,flag), alpha):
 
     return -0.5 * ((flux - f_star) ** 2 / pred_var + np.log(pred_var))
 
-hyperfn = "best_hyperparams.p"
-if not os.path.exists(hyperfn):
-    print("fitting for hyperparams")
-    nll = lambda theta: -ln_likelihood(theta)
 
-    # Optimize
+def test_ln_likelihood(label, f, var, theta_all, f_all, var_all, l_all):
+    print(label)
+    npix = len(f)
+    npix = 150
+    ln_likelihoods = np.zeros(npix)
+    for pix in range(npix):
+        (L,flag), alpha = compute_l_and_alpha(
+                theta_all[pix,:], f_all[:,pix], var_all[:,pix], l_all)
+        ln_likelihoods[pix] = test_ln_likelihood_single_pix(
+                label, f[pix], var[pix], theta_all[pix], (L,flag), alpha, l_all)
+    return sum(ln_likelihoods)
+
+
+def pick_good_obj(pix, f_all, ivar_all, l_all):
+    f = f_all[:, pix]
+    ivar = ivar_all[:, pix]
+
+    # this pixel is bad for the following objects: flux == 0 or flux == 1
+    m = np.logical_and(f != 0.0, f != 1.0)
+    f = f[m]
+    f -= np.mean(f)
+    l = l_all[m,:]
+    var = 1./ivar[m]
+    return f,var,l
+
+
+def train_single_pix(pix, f_all, ivar_all, l_all):
+    f, var, l = pick_good_obj(pix, f_all, ivar_all, l_all)
+    nll = lambda theta: -ln_likelihood(theta, f, var, l)
     bounds = np.log([(0.001, 0.1), (100, 10000), (0.01, 10), (0.01, 10),
                      (0.0001, 0.1)])
     output = op.minimize(nll, np.log([0.01, 100, 0.5, 0.05, 0.01]), 
                          method="L-BFGS-B", bounds=bounds)
     best_hyperparams = output.x
+    print("best hyperparams: ")
+    print(ln_likelihoods[pix])
     print(np.exp(best_hyperparams))
-    pickle.dump(best_hyperparams, open(hyperfn, "wb"), -1)
+    return best_hyperparams
+
+
+def infer_labels_single_pix(pix, best_hyperparams, f_all, var_all, l_all):
+    f, var, l = pick_good_obj(pix, f_all, ivar_all, l_all)
+    hyperparams = best_hyperparams[pix,:]
+    (L,flag), alpha = compute_l_and_alpha(hyperparams, f, var, l)
+    tll = lambda labels: -test_ln_likelihood(labels, f[obj], var[obj], 
+                                             hyperparams, (L,flag), alpha, l)
+    p0 = np.array([np.mean(l[:,i]) for i in range(l.shape[1])])
+    bounds = np.array([(3500,5500), (0,5), (-2.5,0.5)]) # training set dist
+
+    print("fitting for labels")
+    # want to minimize the negative log likelihood (maximize lnp)
+    output = op.minimize(tll, p0, method="L-BFGS-B", bounds=bounds)
+    return output.x
+
+
+def infer_labels_single_obj(obj, hyperparams_all, f_all, var_all, l_all):
+    f = f_all[obj,:]
+    var = var_all[obj,:]
+    # l = l_all[obj,:]
+    tll = lambda labels: -test_ln_likelihood(
+            labels, f, var, hyperparams_all, f_all, var_all, l_all)
+    p0 = np.array([np.mean(l_all[:,i]) for i in range(l_all.shape[1])])
+    bounds = np.array([(3500,5500), (0,5), (-2.5,0.5)]) # training set dist
+    print("fitting for labels")
+    output = op.minimize(tll, p0, method="L-BFGS-B", bounds=bounds)
+    return output
+
+
+f_all = pickle.load(open("data/norm_tr_fluxes.p", "r"))
+l_all = pickle.load(open("data/tr_label_vals.p", "r"))
+ivar_all = pickle.load(open("data/norm_tr_ivars.p", "r"))
+
+# "bad" pixels are those with var == 0 
+keep = np.var(f_all, axis=0) > 0
+f_all = f_all[:,keep]
+ivar_all = ivar_all[:,keep]
+
+npix = f_all.shape[1]
+
+# Covariance function
+
+hyperfn = "best_hyperparams.p"
+if not os.path.exists(hyperfn):
+    print("fitting for hyperparams")
+    # Optimize for each pixel independently
+    npix = f_all.shape[1]
+    best_hyperparams_all = np.zeros((npix, 5))
+    for pix in range(npix):
+        print("training on pixel " + str(pix))
+        best_hyperparams_all[pix,:] = train_single_pix(pix, f_all, ivar_all, l_all) 
+    pickle.dump(best_hyperparams_all, open(hyperfn, "wb"), -1)
 
 else:
     print("loading")
     best_hyperparams = pickle.load(open(hyperfn, "r"))
 
+##### TEST STEP #####
+
 # Now optimize over the test label space
 obj = 0
-(L,flag), alpha = compute_l_and_alpha(best_hyperparams)
-tll = lambda labels: -test_ln_likelihood(labels, f[obj], var[obj], 
-                                         best_hyperparams, (L,flag), alpha)
+print(infer_labels_single_obj(obj, best_hyperparams, f_all, ivar_all, l_all))
 
-p0 = np.array([np.mean(l_all[:,i]) for i in range(l_all.shape[1])])
-bounds = np.array([(3500,5500), (0,5), (-2.5,0.5)]) # training set dist
 
-print("fitting for labels")
-# want to minimize the negative log likelihood (maximize lnp)
-output = op.minimize(tll, p0, method="L-BFGS-B", bounds=bounds)
-
-# test_labels = np.zeros((2000,3)) + np.max(l_all, axis=0)[None,:]
 # axis = 2 
 # test_labels[:,axis] = np.linspace(np.min(l_all[:,axis]), 
 #                                   np.max(l_all[:,axis]), 
